a4ea9bfbb4c436ea1b5d429698313ea6a225200d
Bogdan Ionescu <bogdan.ionescu.work@gmail.com>
2025-02-15 13:18:02 +0000

    fix linux build

--- compile.sh
+++ compile.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-RAYLIB_PATH=/opt/homebrew/Cellar/raylib/5.5
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 CFLAGS=$(pkg-config --cflags raylib)
 LDFLAGS=$(pkg-config --libs raylib)
 
 mkdir -p build &&
 cc main.c -g -std=c99 -c ${CFLAGS} -o build/main.o &&
-cc build/main.o -s -Wall -std=c99 ${CFLAGS} -L/usr/local/lib/ ${LDFLAGS} -o build/cginc &&
+cc build/main.o -s -Wall -std=c99 ${CFLAGS} -L/usr/local/lib/ ${LDFLAGS} -lm -o build/cginc &&
 ./build/cginc test.nc resources/test.stl
